d0f5619aa53204f9f9dd5594bb087880daf00803
AlpayCa <alpay41@gmail.com>
2023-04-29 20:24:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OgrenciEklemeRastgeleSecme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kaç Öğrenci Eklenecek ?");
+            int eklenecekOgrenciSayisi = int.Parse(Console.ReadLine());
+
+            string[] ogrenciAdi = new string[eklenecekOgrenciSayisi];
+            string[] ogrenciSoyadi = new string[eklenecekOgrenciSayisi];
+
+            for (int i = 0; i < eklenecekOgrenciSayisi; i++)//Diziye eleman eklemek
+            {
+                Console.WriteLine("{0}. Öğrenci Adını Giriniz",i+1);
+                ogrenciAdi[i] += Console.ReadLine();
+                Console.WriteLine("{0}. Öğrenci Soyadını Giriniz", i+1);
+                ogrenciSoyadi[i] += Console.ReadLine();
+            }
+            Random rnd = new Random();
+            int secilen = rnd.Next(0,eklenecekOgrenciSayisi);
+            Console.WriteLine("Rastgele Seçilen Öğrenci :{0} {1}", ogrenciAdi[secilen], ogrenciSoyadi[secilen]);
+        }
+    }
+}
